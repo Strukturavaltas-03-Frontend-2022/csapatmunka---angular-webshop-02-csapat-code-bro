7a6ad7945bf6ec0392165de0c959c7d59afbed7a
Istvan888 <marai.istvanp@gmail.com>
2022-11-13 16:20:13 +0100

    ez megy

--- src/app/page/cat03/cat03.component.ts
+++ src/app/page/cat03/cat03.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Product } from 'src/app/model/product';
+import { ProductService } from 'src/app/service/product.service';
 
 @Component({
   selector: 'app-cat03',
@@ -7,7 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Cat03Component implements OnInit {
 
-  constructor() { }
+
+  comedyMovies$: Observable<Product[]> = this.productService
+  .getAll()
+  .pipe(map((product) => product.filter((movie) => movie.catId === 3)));
+
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
   }
