0fea0a7dbf3f85c3641229880db68030e68b1cd1
k3csk3 <szilagyi.viktor89@gmail.com>
2022-11-16 22:18:15 +0100

    paginator

--- src/app/common/product-list/product-list.component.ts
+++ src/app/common/product-list/product-list.component.ts
@@ -10,11 +10,46 @@ import { ProductService } from 'src/app/service/product.service';
 export class ProductListComponent implements OnInit {
   constructor(private productService: ProductService) {}
 
+  allPage: number[] = [];
+
+  page: number = 1;
+
   productList: Product[] = [];
 
   ngOnInit(): void {
+    this.refreshPage();
+
+    this.productService.getAll().subscribe((datalist) => {
+      for (let i = 0; i < Math.ceil(datalist.length / 10); i++) {
+        this.allPage.push(i + 1);
+      }
+    });
+  }
+
+  onPage(page: number): void {
+    this.page = page;
+    this.refreshPage();
+  }
+
+  prevPage(): void {
+    if (this.page > 1) {
+      this.page = this.page - 1;
+    }
+    this.refreshPage();
+  }
+
+  nextPage(): void {
+    if (this.page !== this.allPage.length) {
+      this.page = this.page + 1;
+    }
+    this.refreshPage();
+  }
+
+  refreshPage(): void {
     this.productService
-      .getAll()
+      .getAll(`?_page=${this.page}&_limit=10`)
       .subscribe((dataList) => (this.productList = dataList));
   }
+
+  //Greg brutál supportjával
 }
